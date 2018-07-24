ba99db206eba287bec41950555e4d253f7337545
unknown <usvbox@gmail.com>
2018-07-23 23:46:18 -0700

    update

---
+++ magic_counter/magic_counter.js
@@ -0,0 +1,28 @@
+function makeCounter () {
+    var count = 0;
+    function counter () {
+        count = count + 1;
+        return count;
+    }
+    return counter;
+}
+var doCount = makeCounter ();
+console.log (doCount ());
+console.log (doCount ());
+console.log (doCount ());
+
+function makePassword (password) {
+    return function guessPassword (passwordGuess) {
+        return (passwordGuess === password);
+    };
+}
+
+var tryGuess = makePassword ("secret");
+console.log ("Guessing 'nope':" + tryGuess("nope"));
+console.log ("Guessing 'secret':" + tryGuess("secret"));
+
+function multN (n) {
+    return function multiplier (number) {
+        return number * n;
+    };
+}
