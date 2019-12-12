c238f5e30650cc9684d970c1e323d585c2f49a02
romanusov <usvbox@gmail.com>
2019-12-12 18:59:11 +0300

    completed lesson 50

--- complete-javascript-course-master/4-DOM-pig-game/starter/app.js
+++ complete-javascript-course-master/4-DOM-pig-game/starter/app.js
@@ -8,3 +8,66 @@ GAME RULES:
 - The first player to reach 100 points on GLOBAL score wins the game
 
 */
+
+var scores, roundScore, activePlayer;
+
+overallScores = [0,0];
+roundScore = 0;
+activePlayer = 0;
+
+
+//this is to be able to assign a round score value for the active player based on the dice value
+//document.querySelector('#current-' + activePlayer).textContent = dice; //this is a setter for placing text in HTML
+//document.querySelector('#current-' + activePlayer).innerHTML = '<em>' + dice + '</em>'; //this is a setter for placing html code within HTML
+
+//this is to be able to hide the dice from the screen at the start of the game
+document.querySelector('.dice').style.display = 'none'; //changes value of a css element
+document.getElementById('score-0').textContent = '0';
+document.getElementById('score-1').textContent = '0';
+document.getElementById('current-0').textContent = '0';
+document.getElementById('current-1').textContent = '0';
+
+
+//var x = document.querySelector('#score-0').textContent; //this is a getter to get a value from an html element
+//console.log(x);
+
+//this is to process a click event upon clicking the roll dice button
+
+
+//here we use an anonymous function (it's appropriate here as we use it just for handling the click event), which is also a callback function
+//because it will be called for us by the addEventListener method that will be listening for a click event in the element of btn-roll class
+
+document.querySelector('.btn-roll').addEventListener('click', function() { 
+
+    // 1. Random number
+
+    //this is to assign randomizer to the dice variable
+    var dice = Math.floor(Math.random() * 6) + 1; //Math.random gives a random between 0 and 1; multiplying by 6 => random between 0 and 5; Math.floor => skip decimals
+
+    // 2. Display result
+    var diceDOM = document.querySelector('.dice'); //here we assign an html element to a variable so that we can reuse it easily
+    diceDOM.style.display = 'block';
+    diceDOM.src = 'dice-' + dice + '.png';
+
+    // 3. Update the round score IF the rolled number was NOT a 1
+    if (dice !== 1) {
+        //Add score
+        roundScore += dice;
+        document.querySelector('#current-' + activePlayer).textContent = roundScore;
+    } else {
+        //Next player
+        activePlayer === 0 ? activePlayer = 1 : activePlayer = 0; //ternary operator for simple if statements
+        roundScore = 0;
+        document.getElementById('current-0').textContent = '0';
+        document.getElementById('current-1').textContent = '0';
+
+        document.querySelector('.player-0-panel').classList.toggle('active'); //adds or removes parameter to class in html document - if it's there - remove, if not add
+        document.querySelector('.player-1-panel').classList.toggle('active');
+
+        //document.querySelector('.player-0-panel').classList.remove('active');
+        //document.querySelector('.player-1-panel').classList.add('active');
+
+        document.querySelector('.dice').style.display = 'none';
+    }
+
+});
