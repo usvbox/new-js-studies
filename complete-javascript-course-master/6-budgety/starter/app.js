95ae5aabdb1109a32606d2327266e045bd5d501a
Roman Usov <romanusov@Romans-MacBook-Pro.local>
2020-02-09 14:28:19 +0300

    completed lesson 80

--- complete-javascript-course-master/6-budgety/starter/app.js
+++ complete-javascript-course-master/6-budgety/starter/app.js
@@ -5,49 +5,60 @@
 
 
  //UI CONTROLLER
- var UIController = (function() {
-    var DOMStrings = {
+ var uiController = (function() {
+    var domStrings = {
         inputType: '.add__type',
-        description: '.add__description',
-        value: '.add__value',
-        addButton: '.add__btn'
+        inputDescription: '.add__description',
+        inputValue: '.add__value',
+        inputButton: '.add__btn'
     };
     return {
         getInput: function() {
             return {
-                type: document.querySelector(DOMStrings.inputType).value, // will be either inc or exp
-                description: document.querySelector(DOMStrings.description).value,
-                value: document.querySelector(DOMStrings.value).value
+                type: document.querySelector(domStrings.inputType).value, // will be either inc or exp
+                description: document.querySelector(domStrings.inputDescription).value,
+                value: document.querySelector(domStrings.inputValue).value
             };
         },
-        getDOMStrings: function() {
-            return DOMStrings;
+        getDomStrings: function() {
+            return domStrings;
         }
     }
  })();
 
 
 //LOGIC CONTROLLER
- var controller = (function(budgetCtrl, UICtrl) {
-    var DOM = UICtrl.getDOMStrings();
+ var controller = (function(budgetCtrl, uiCtrl) {
+    
+    var setupEventListeners = function () {
+        var dom = uiCtrl.getDomStrings();
+        document.querySelector(dom.inputButton).addEventListener('click', ctrlAddItem);
+        document.addEventListener('keypress', function(event) {
+            if (event.keyCode === 13 || event.which === 13) {
+                ctrlAddItem();
+            }
+         });
+    };
+    
     var ctrlAddItem = function () {
         // 1. get the field input data
-        var input = UICtrl.getInput();
+        var input = uiCtrl.getInput();
         console.log(input);
         // 2. add the item to the budget controller
         // 3. add the item to the UI 
         // 4. calculate the budget
         // 5. display the budget on the UI
-    }
-    
-    document.querySelector(DOM.addButton).addEventListener('click', ctrlAddItem);
+    };
 
-    document.addEventListener('keypress', function(event) {
-       if (event.keyCode === 13 || event.which === 13) {
-           ctrlAddItem();
-       }
-    });
+    return {
+        init: function () {
+            console.log('Application is launched.');
+            setupEventListeners();
+        }
+    };
 
- })(budgetController, UIController);
+ })(budgetController, uiController);
+
+ controller.init();
 
 
