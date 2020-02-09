34fd32561e1e17ffd68462e48e2fd1c2f86d252c
Roman Usov <romanusov@Romans-MacBook-Pro.local>
2020-02-09 14:32:52 +0300

    test

--- complete-javascript-course-master/6-budgety/starter/app.js
+++ complete-javascript-course-master/6-budgety/starter/app.js
@@ -37,13 +37,12 @@
             if (event.keyCode === 13 || event.which === 13) {
                 ctrlAddItem();
             }
-         });
+         }); 
     };
     
     var ctrlAddItem = function () {
         // 1. get the field input data
         var input = uiCtrl.getInput();
-        console.log(input);
         // 2. add the item to the budget controller
         // 3. add the item to the UI 
         // 4. calculate the budget
