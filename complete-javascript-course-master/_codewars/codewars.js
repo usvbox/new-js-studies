6330c02a6f6e9576fc6b3a3cc1807cafd2a3cb50
Roman Usov <usvbox@yandex.ru>
2021-03-23 22:00:17 +0300

    completed codewars challenge

--- complete-javascript-course-master/_codewars/codewars.js
+++ complete-javascript-course-master/_codewars/codewars.js
@@ -1,11 +1,4 @@
-// Remember, we're gonna use strict mode in all scripts now!
-'use strict';
-
-// const x = '23';
-
-// const calcAge = birthYear => 2037 - birthYear;
-// console.log(calcAge(1991));
-
+//School Paperwork
 //Your classmates asked you to copy some paperwork for them. You know that there are 'n' classmates and the paperwork has 'm' pages.
 //Your task is to calculate how many blank pages do you need. If n < 0 or m < 0 return 0.
 
