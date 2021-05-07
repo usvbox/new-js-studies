5b140b3640790a8c583d26b832e22eeaee1635f0
Roman Usov <usvbox@yandex.ru>
2021-05-07 20:31:16 +0300

    completed a lesson on manipulating element classes with javascript

--- complete-javascript-course-master/06-Modal/starter/script.js
+++ complete-javascript-course-master/06-Modal/starter/script.js
@@ -1,1 +1,24 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const closeModalButton = document.querySelector('.close-modal');
+const openModalButtons = document.querySelectorAll('.show-modal');
+
+const openModal = function () {
+	modal.classList.remove('hidden');
+	overlay.classList.remove('hidden');
+};
+const closeModal = function () {
+	modal.classList.add('hidden');
+	overlay.classList.add('hidden');
+};
+
+console.log(openModalButtons);
+
+for (let i = 0; i < openModalButtons.length; i++) {
+	openModalButtons[i].addEventListener('click', openModal);
+}
+
+closeModalButton.addEventListener('click', closeModal);
+overlay.addEventListener('click', closeModal);
