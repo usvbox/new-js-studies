70ca64feadae36a5c0eee2ccd0451b5afbff72bb
Roman Usov <usvbox@yandex.ru>
2021-05-10 20:33:48 +0300

    completed lesson 82

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+
+// Create variables for the elements to be used in the code below
+
+const diceEl = document.querySelector('.dice');
+const playerOneScoreEl = document.querySelector('#score--0');
+const playerTwoScoreEl = document.querySelector('#score--1');
+
+// Create small reusable functions to manipulate the DOM elements
+
+const hideElement = function (el) {
+  el.classList.add('hidden');
+};
+
+const setScoresToZero = function (score1, score2) {
+  score1.textContent = 0;
+  score2.textContent = 0;
+};
+
+// Set the game starting state
+
+hideElement(diceEl);
+setScoresToZero(playerOneScoreEl, playerTwoScoreEl);
