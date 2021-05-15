f4a6d41c63dc93b2ea27490fb44b10a2ba059b33
Roman Usov <usvbox@yandex.ru>
2021-05-15 23:45:59 +0300

    completed lesson 85

--- complete-javascript-course-master/07-Pig-Game/starter/script.js
+++ complete-javascript-course-master/07-Pig-Game/starter/script.js
@@ -2,17 +2,20 @@
 
 // Create game variables
 
-let playerOneGameScore = 0;
-let playerTwoGameScore = 0;
-let playerOneScore = 0;
-let playerTwoScore = 0;
+const gameScores = [0, 0];
+let activePlayer = 0;
 let currentScore = 0;
+const winningScore = 20;
+let gamePlaying = true;
+
+// let playerOneScore = 0;
+// let playerTwoScore = 0;
 
 // Create variables for the elements to be used in the code below
 
 const diceEl = document.querySelector('.dice');
-const playerOneGameScoreEl = document.querySelector('#score--0');
-const playerTwoGameScoreEl = document.querySelector('#score--1');
+const playerOneGameScoreEl = document.getElementById('score--0');
+const playerTwoGameScoreEl = document.getElementById('score--1');
 const playerOneCurrentScoreEl = document.querySelector('#current--0');
 const playerTwoCurrentScoreEl = document.querySelector('#current--1');
 const rollDiceBtnEl = document.querySelector('.btn--roll');
@@ -20,8 +23,7 @@ const newGameBtnEl = document.querySelector('.btn--new');
 const holdBtnEl = document.querySelector('.btn--hold');
 const playerOneEl = document.querySelector('.player--0');
 const playerTwoEl = document.querySelector('.player--1');
-const activePlayerEl = document.querySelector('.player--active');
-//const currentScoreEl = activePlayerEl.querySelector('.current-score');
+const players = document.querySelectorAll('.player');
 
 // Create small reusable functions to manipulate the DOM elements
 
@@ -43,6 +45,109 @@ const rollTheDice = function () {
   return dice;
 };
 
+// two implementation option for switching players - simple and with forEach() method
+const switchPlayers = function () {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    gameScores[activePlayer];
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  playerOneEl.classList.toggle('player--active');
+  playerTwoEl.classList.toggle('player--active');
+};
+
+const switchPlayer = function (players) {
+  currentScore = 0;
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  document.getElementById(`score--${activePlayer}`).textContent =
+    gameScores[activePlayer];
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  players.forEach(player => {
+    player.classList.toggle('player--active');
+  });
+};
+
+// const endTheGame = function () {
+//   document.getElementById(`score--${activePlayer}`).textContent =
+//     gameScores[activePlayer];
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.add('player--winner');
+//   document
+//     .querySelector(`.player--${activePlayer}`)
+//     .classList.remove('player--active');
+//   document.getElementById(`name--${activePlayer}`).textContent = 'Winner!';
+//   hideElement(diceEl);
+//   rollDiceBtnEl.removeEventListener('click', clickRollDice);
+//   holdBtnEl.removeEventListener('click', clickHold);
+//   rollDiceBtnEl.disabled = true;
+//   holdBtnEl.disabled = true;
+// };
+
+const endTheGame = function () {
+  document.getElementById(`score--${activePlayer}`).textContent =
+    gameScores[activePlayer];
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.add('player--winner');
+  document
+    .querySelector(`.player--${activePlayer}`)
+    .classList.remove('player--active');
+  document.getElementById(`name--${activePlayer}`).textContent = 'Winner!';
+  hideElement(diceEl);
+  gamePlaying = false;
+};
+
+// const clickRollDice = function () {
+//   const diceRoll = rollTheDice();
+//   unhideElement(diceEl);
+//   diceEl.setAttribute('src', `dice-${diceRoll}.png`);
+//   if (diceRoll > 1) {
+//     currentScore += diceRoll;
+//     document.getElementById(`current--${activePlayer}`).textContent =
+//       currentScore;
+//   }
+//   if (diceRoll === 1) {
+//     switchPlayer(players);
+//   }
+// };
+
+const clickRollDice = function () {
+  if (gamePlaying) {
+    const diceRoll = rollTheDice();
+    unhideElement(diceEl);
+    diceEl.setAttribute('src', `dice-${diceRoll}.png`);
+    if (diceRoll > 1) {
+      currentScore += diceRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    }
+    if (diceRoll === 1) {
+      switchPlayer(players);
+    }
+  }
+};
+
+// const clickHold = function () {
+//   gameScores[activePlayer] += currentScore;
+//   if (gameScores[activePlayer] < winningScore) {
+//     switchPlayer(players);
+//   } else {
+//     endTheGame();
+//   }
+// };
+
+const clickHold = function () {
+  if (gamePlaying) {
+    gameScores[activePlayer] += currentScore;
+    if (gameScores[activePlayer] < winningScore) {
+      switchPlayer(players);
+    } else {
+      endTheGame();
+    }
+  }
+};
+
 // Set the game starting state
 
 hideElement(diceEl);
@@ -50,42 +155,37 @@ setScoresToZero(playerOneGameScoreEl, playerTwoGameScoreEl);
 
 // Roll the dice
 
-// 1. select Roll Dice element and attach a click event listener to it that will call the rollTheDice function
+rollDiceBtnEl.addEventListener('click', clickRollDice);
 
-rollDiceBtnEl.addEventListener('click', function () {
-  const diceRoll = rollTheDice();
-  unhideElement(diceEl);
-  diceEl.setAttribute('src', `dice-${diceRoll}.png`);
+// Hold the score
 
-  if (diceRoll > 1) {
-    if (playerOneEl.classList.contains('player--active')) {
-      playerOneScore += diceRoll;
-      playerOneCurrentScoreEl.textContent = playerOneScore;
-    } else if (playerTwoEl.classList.contains('player--active')) {
-      playerTwoScore += diceRoll;
-      playerTwoCurrentScoreEl.textContent = playerTwoScore;
-    }
-  } else {
-    if (playerOneEl.classList.contains('player--active')) {
-      playerOneScore = 0;
-      playerOneCurrentScoreEl.textContent = playerOneScore;
-      playerOneEl.classList.toggle('player--active');
-      playerTwoEl.classList.toggle('player--active');
-    } else if (playerTwoEl.classList.contains('player--active')) {
-      playerTwoScore = 0;
-      playerTwoCurrentScoreEl.textContent = playerTwoScore;
-      playerOneEl.classList.toggle('player--active');
-      playerTwoEl.classList.toggle('player--active');
-    }
-  }
+holdBtnEl.addEventListener('click', clickHold);
 
-  // if (diceRoll > 1) {
-  //   currentScore += diceRoll;
-  //   currentScoreEl.textContent = currentScore;
-  // } else {
-  //   currentScore = 0;
-  //   currentScoreEl.textContent = currentScore;
-  //   playerOneEl.classList.toggle('player--active');
-  //   playerTwoEl.classList.toggle('player--active');
-  // }
-});
+// Initial version of the code switching the players
+
+/*   rollDiceBtnEl.addEventListener('click', function () {
+    const diceRoll = rollTheDice();
+    unhideElement(diceEl);
+    diceEl.setAttribute('src', `dice-${diceRoll}.png`);
+
+    if (diceRoll > 1) {
+      if (playerOneEl.classList.contains('player--active')) {
+        playerOneScore += diceRoll;
+        playerOneCurrentScoreEl.textContent = playerOneScore;
+      } else if (playerTwoEl.classList.contains('player--active')) {
+        playerTwoScore += diceRoll;
+        playerTwoCurrentScoreEl.textContent = playerTwoScore;
+      }
+    } else {
+      if (playerOneEl.classList.contains('player--active')) {
+        playerOneScore = 0;
+        playerOneCurrentScoreEl.textContent = playerOneScore;
+        playerOneEl.classList.toggle('player--active');
+        playerTwoEl.classList.toggle('player--active');
+      } else if (playerTwoEl.classList.contains('player--active')) {
+        playerTwoScore = 0;
+        playerTwoCurrentScoreEl.textContent = playerTwoScore;
+        playerOneEl.classList.toggle('player--active');
+        playerTwoEl.classList.toggle('player--active');
+      }
+    } */
---
+++ complete-javascript-course-master/_youtube-lessons/event-listeners/script.js
@@ -0,0 +1,17 @@
+const divs = document.querySelectorAll('div');
+
+function addGlobalEventListener(type, selector, callback) {
+	document.addEventListener(type, (e) => {
+		if (e.target.matches(selector)) callback(e);
+	});
+}
+
+addGlobalEventListener('click', 'div', (e) => {
+	console.log('hi');
+});
+
+const newDiv = document.createElement('div');
+newDiv.style.width = 200 + 'px';
+newDiv.style.height = 200 + 'px';
+newDiv.style.backgroundColor = 'purple';
+document.body.append(newDiv);
