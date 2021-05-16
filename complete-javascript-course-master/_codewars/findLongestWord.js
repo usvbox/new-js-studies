8cfb31a882f86938aff306e267637d2e08f0bc7c
Roman Usov <usvbox@yandex.ru>
2021-05-16 15:18:22 +0300

    completed the squash the bugs challenge

---
+++ complete-javascript-course-master/_codewars/findLongestWord.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const stringToProcess = 'Take me to tinseltown with you';
+
+function findLongest(str) {
+	var spl = str.split(' ');
+	var longest = 0;
+	for (var i = 0; i < spl.length; i++) {
+		if (spl[i].length > longest) {
+			longest = spl[i].length;
+		}
+	}
+	return longest;
+}
+
+const longestWord = findLongest(stringToProcess);
+console.log(longestWord);
