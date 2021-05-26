909e9eead5d3de386a21f41d4fe29b738895df17
Roman Usov <usvbox@yandex.ru>
2021-05-27 00:10:32 +0300

    completed coding challenge for Every Possbile Sum and installed jest

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -1,6 +1,8 @@
 'use strict';
 
-function calcAge(birthYear) {
+//The Scope Chain
+
+/* function calcAge(birthYear) {
   const age = 2037 - birthYear;
   function printAge() {
     let output = `${firstName}, you are ${age}, born in ${birthYear}.`;
@@ -26,3 +28,62 @@ function calcAge(birthYear) {
 const firstName = 'Jonas';
 calcAge(1991);
 //console.log(millenial);
+
+testVar;
+console.log(testVar);
+
+var testVar = 5; */
+
+//Hoisting
+
+//Variables
+/* console.log(me);
+console.log(job);
+console.log(year);
+
+var me = 'Jonas';
+let job = 'teacher';
+const year = 1991; */
+
+//Functions
+/* console.log(addDecl(2, 3));
+console.log(addExpr(2, 3));
+console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+  return a + b;
+}
+
+var addExpr = function (a, b) {
+  return a + b;
+};
+
+const addArrow = (a, b) => a + b; */
+
+//Example demonstrating the key pitfall of hoisting - a behavior we can get when not careful with using var-type variables
+
+if (!numProducts) deleteShoppingCart();
+
+var numProducts = 10;
+
+function deleteShoppingCart() {
+  console.log('All products deleted');
+}
+
+// var testVariable = 0;
+// function checkFalsy(variable) {
+//   if (variable) {
+//     return 'var is not zero';
+//   }
+//   if (!variable) {
+//     return 'var is zero';
+//   }
+// }
+
+// console.log(checkFalsy(testVariable));
+
+var x = 1;
+let y = 2;
+const z = 3;
+
+console.log(x === window.x);
---
+++ complete-javascript-course-master/_codewars/every-possible-sum-of-two-digits.js
@@ -0,0 +1,31 @@
+'use strict';
+
+const testNumber = [3541];
+const testArraySumCombinations = [3 + 5, 3 + 4, 3 + 1, 5 + 4, 5 + 1, 4 + 1];
+const testArraySums = [8, 7, 4, 9, 6, 5];
+
+const inputNumber = [12345];
+
+const giveAllSumsOfTwoDigits = function (num) {
+	// const inputArr = num.toString().split('');
+	// console.log(inputArr);
+
+	let inputArr = [];
+	let outputArr = [];
+
+	num
+		.toString() //first convert the input number to a string
+		.split('') //then split the resulting string with numbers into an array of substrings
+		.forEach((el) => inputArr.push(Number(el))); //finally convert each substring into a number and push it into a new inputArr array
+
+	for (let i = 0; i < inputArr.length - 1; i++) {
+		for (let j = i + 1; j < inputArr.length; j++) {
+			outputArr.push(inputArr[i] + inputArr[j]);
+		}
+	}
+
+	return outputArr;
+};
+
+const output = giveAllSumsOfTwoDigits(inputNumber);
+console.log(output);
