b3f752c7f1684f5b64e22c0056be6cabd5f956c3
Roman Usov <usvbox@yandex.ru>
2021-05-27 19:11:36 +0300

    completed the Vowel Count challenge

--- complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
+++ complete-javascript-course-master/08-Behind-the-Scenes/starter/script.js
@@ -62,13 +62,13 @@ const addArrow = (a, b) => a + b; */
 
 //Example demonstrating the key pitfall of hoisting - a behavior we can get when not careful with using var-type variables
 
-if (!numProducts) deleteShoppingCart();
+// if (!numProducts) deleteShoppingCart();
 
-var numProducts = 10;
+// var numProducts = 10;
 
-function deleteShoppingCart() {
-  console.log('All products deleted');
-}
+// function deleteShoppingCart() {
+//   console.log('All products deleted');
+// }
 
 // var testVariable = 0;
 // function checkFalsy(variable) {
@@ -82,8 +82,47 @@ function deleteShoppingCart() {
 
 // console.log(checkFalsy(testVariable));
 
-var x = 1;
-let y = 2;
-const z = 3;
+// var x = 1;
+// let y = 2;
+// const z = 3;
 
-console.log(x === window.x);
+// console.log(x === window.x);
+
+// this keyword
+
+console.log(this);
+
+const calcAge = function (birthYear) {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAge(1991);
+
+const calcAgeArrow = birthYear => {
+  console.log(2037 - birthYear);
+  console.log(this);
+};
+
+calcAgeArrow(1991);
+
+const jonas = {
+  year: 1991,
+  calcAge: function () {
+    console.log(this);
+    console.log(2037 - this.year);
+  },
+};
+
+jonas.calcAge();
+
+const matilda = {
+  year: 2017,
+};
+
+matilda.calcAge = jonas.calcAge;
+
+matilda.calcAge();
+
+const f = jonas.calcAge;
+f();
---
+++ complete-javascript-course-master/_codewars/vowel-count.js
@@ -0,0 +1,53 @@
+'use strict';
+
+const inputString = 'trash can sail see';
+const expectedVowelCount = 5;
+
+const stringIntoArray = inputString.split('');
+console.log(stringIntoArray);
+console.log(inputString);
+
+// const countVowels = function (str) {
+// 	const vowels = ['a', 'e', 'i', 'o', 'u'];
+// 	let counter = 0;
+// 	for (let i = 0; i < vowels.length; i++) {
+// 		for (let j = 0; j < str.length; j++) {
+// 			if (vowels[i] === str[j]) {
+// 				counter += 1;
+// 			} else {
+// 				continue;
+// 			}
+// 		}
+// 	}
+// 	return counter;
+// };
+
+const countVowels = function (str) {
+	const vowels = ['a', 'e', 'i', 'o', 'u'];
+	let counter = 0;
+	vowels.forEach((el) => {
+		for (let j = 0; j < str.length; j++) {
+			if (el === str[j]) {
+				counter += 1;
+			} else {
+				continue;
+			}
+		}
+	});
+	return counter;
+};
+
+const outputVowelCount = countVowels(inputString);
+console.log(outputVowelCount);
+
+const countVowelsNext = function (str) {
+	const vowels = 'aeiou';
+	// const strToArray = str.split('');
+	// console.log(strToArray);
+	// const arrWithVowelsOnly = strToArray.filter((el) => vowels.includes(el));
+	// console.log(arrWithVowelsOnly);
+	// console.log(arrWithVowelsOnly.length);
+	return str.split('').filter((el) => vowels.includes(el)).length;
+};
+
+countVowelsNext(inputString);
