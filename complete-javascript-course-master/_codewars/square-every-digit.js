124d0c804f029a3c4b1ab9295e894d2a17a8deae
Roman Usov <usvbox@yandex.ru>
2021-05-31 19:15:58 +0300

    completed the Square Every Digit Challenge

---
+++ complete-javascript-course-master/_codewars/square-every-digit.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const test = '234524';
+
+console.log(test ** 2);
+
+const inputNumber = 9119;
+
+// const squareDigits = function (num) {
+// 	let calcResults = [];
+// 	const numAsStr = num.toString();
+// 	for (let i = 0; i < numAsStr.length; i++) {
+// 		calcResults.push((Number(numAsStr[i]) ** 2).toString());
+// 	}
+// 	return Number(calcResults.join(''));
+// };
+
+const squareDigits = function (num) {
+	let calcResults = [];
+	const numAsStr = num.toString();
+	for (let i = 0; i < numAsStr.length; i++) {
+		calcResults.push(numAsStr[i] ** 2);
+	}
+	return Number(calcResults.join(''));
+};
+
+const outputNumber = squareDigits(test);
+console.log(outputNumber);
