bba43d67860b268b4ab9cd79d3aa6959acbf51b5
Roman Usov <usvbox@yandex.ru>
2021-06-03 20:57:00 +0300

    completed lessons 110, 111 plus the you're square challenge

--- complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
+++ complete-javascript-course-master/09-Data-Structures-Operators/starter/script.js
@@ -1,49 +1,59 @@
 'use strict';
 
+const weekdays = ['Sun', 'Mon', 'Tue', 'Thu', 'Fri', 'Sat', 'Sun'];
+
+const openingHours = {
+  [weekdays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekdays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [`Day ${2 + 4}`]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
   categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (starterIndex, mainIndex) {
+  openingHours,
+  order(starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
-
-  orderDelivery: function ({
-    starterIndex = 1,
-    mainIndex = 0,
-    time = '20:00',
-    address,
-  }) {
+  orderDelivery({ starterIndex = 1, mainIndex = 0, time = '20:00', address }) {
     console.log(
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} by ${time}.`
     );
   },
-  orderPasta: function (ing1, ing2, ing3) {
+  orderPasta(ing1, ing2, ing3) {
     console.log(
       `Here's your delicious pasta with ${ing1}, ${ing2}, and ${ing3}.`
     );
   },
-  orderPizza: function (mainIngredient, ...otherIngredients) {
+  orderPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
+
+console.log(restaurant);
+
+// const menu = [...restaurant.starterMenu, ...restaurant.mainMenu];
+
+// for (const item of menu) console.log(item);
+
+// for (const [i, el] of menu.entries()) {
+//   console.log(`${i + 1}: ${el}`);
+// }
+
+// console.log([...menu.entries()]);
 
 // restaurant.orderDelivery({
 //   time: '22:30',
@@ -219,5 +229,5 @@ const restaurant = {
 // restaurant.orderPizza && restaurant.orderPizza('mushrooms', 'onions');
 
 //restaurant.numGuests = 0;
-const guests = restaurant.numGuests ?? 10;
-console.log(guests);
+// const guests = restaurant.numGuests ?? 10;
+// console.log(guests);
---
+++ complete-javascript-course-master/_codewars/you-are-square.js
@@ -0,0 +1,12 @@
+'use strict';
+
+console.log(Number.isInteger(Math.sqrt(-2)));
+
+const inputNumber = [25, -1, 0, 33, 7, 49];
+
+const isSquare = function (num) {
+	return Number.isInteger(Math.sqrt(num));
+};
+
+const outputNumber = isSquare(inputNumber[5]);
+console.log(outputNumber);
