eceeaeba89abd4a0dd3b9344bc51677ae72ea2ce
Roman Usov <usvbox@yandex.ru>
2021-06-03 19:40:00 +0300

    completed isogram challenge

---
+++ complete-javascript-course-master/_codewars/get-the-middle-character.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// const inputString = 'testing';
+
+// const getMiddleCharacter = function (str) {
+// 	function isEven(num) {
+// 		return !(num % 2) ? true : false;
+// 	}
+// 	let output = '';
+// 	if (isEven(str.length)) {
+// 		output += str[str.length / 2 - 1] + str[str.length / 2];
+// 	} else {
+// 		output += str[Math.trunc(str.length / 2)];
+// 	}
+// 	console.log(output);
+// 	return output;
+// };
+
+// getMiddleCharacter(inputString);
+
+// function isEven(num) {
+// 	return !(num % 2) ? true : false;
+// }
+
+// const test = isEven(9);
+// console.log(test);
+
+const test2 = 'press';
+
+// console.log(test2[test2.length / 2 + 1]);
+// console.log(test2.slice((test2.length - 1) / 2, test2.length / 2 + 1));
+
+function getMiddle(str) {
+	console.log((str.length - 1) / 2);
+	console.log(str.length / 2 + 1);
+	console.log(str.slice(str.length / 2 + 1));
+	return str.slice((str.length - 1) / 2, str.length / 2 + 1);
+}
+
+console.log(getMiddle(test2));
---
+++ complete-javascript-course-master/_codewars/isogram.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const inputStr = ['Dermatoglyphics', 'aba', 'moOse', ''];
+
+// const checkIfIsogram = function (str) {
+// 	if (str.length === 0) return true;
+// 	const strToArray = str.toLowerCase().split('');
+// 	for (let i = 0; i < strToArray.length; i++) {
+// 		if (strToArray.indexOf(strToArray[i]) !== i) return false;
+// 	}
+// 	return true;
+// };
+
+const checkIfIsogram = function (str) {
+	str = str.toLowerCase();
+	for (let i = 0; i < str.length; i++) {
+		console.log(str.indexOf(str[i], i + 1));
+		if (str.indexOf(str[i], i + 1) > i) return false;
+	}
+	return true;
+};
+
+const output = checkIfIsogram(inputStr[3]);
+console.log(output);
+
+// const test = '';
+// console.log(test.indexOf(test[0], 1));
