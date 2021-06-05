f70ead471b73c6a333579d5bc14824198efc13ce
Roman Usov <usvbox@yandex.ru>
2021-06-05 21:28:42 +0300

    completed x-s and o-s challenge and complementary DNA challenge

---
+++ complete-javascript-course-master/_codewars/complementary-dna.js
@@ -0,0 +1,30 @@
+'use strict';
+
+const inputStrand = 'ATTGC';
+
+// function DNAStrand(strand) {
+// 	const DNAMap = new Map([
+// 		['A', 'T'],
+// 		['T', 'A'],
+// 		['C', 'G'],
+// 		['G', 'C'],
+// 	]);
+// 	let outputStrand = '';
+// 	for (const char of strand) {
+// 		outputStrand += DNAMap.get(char);
+// 	}
+// 	return outputStrand;
+// }
+
+function DNAStrand(strand) {
+	const DNAMap = {
+		A: 'T',
+		T: 'A',
+		C: 'G',
+		G: 'C',
+	};
+	return [...strand].map((el) => DNAMap[el]).join('');
+}
+
+const outputStrand = DNAStrand(inputStrand);
+console.log(outputStrand);
---
+++ complete-javascript-course-master/_codewars/exes-and-ohs.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const inputStr = 'xxoo';
+
+console.log(inputStr.split('x'));
+
+// const xo = function (str) {
+// 	const inputArr = [...str.toLowerCase()];
+// 	console.log(inputArr);
+// 	function filterValue(val, arr) {
+// 		return arr.filter((el) => val === el);
+// 	}
+// 	const mappedStr = new Map([
+// 		['x', filterValue('x', inputArr)],
+// 		['o', filterValue('o', inputArr)],
+// 	]);
+// 	console.log(mappedStr);
+// 	return mappedStr.get('x').length === mappedStr.get('o').length ? true : false;
+// };
+
+const xo = function (str) {
+	const x = [...str.toLowerCase()].filter((el) => el === 'x');
+	const o = [...str.toLowerCase()].filter((el) => el === 'o');
+	return x.length === o.length ? true : false;
+};
+
+const output = xo(inputStr);
+console.log(output);
+
+const test = ['a', 'b', 'c', 'a'];
+function filterValue(val, arr) {
+	return arr.filter((el) => val === el);
+}
+
+console.log(filterValue('a', test));
