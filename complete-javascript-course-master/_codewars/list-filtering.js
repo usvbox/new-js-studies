0115b08e40917012bf0228a9a3aaf898d834ef79
Roman Usov <usvbox@yandex.ru>
2021-06-06 22:47:47 +0300

    completed the list filtering and the maskify challenges

---
+++ complete-javascript-course-master/_codewars/list-filtering.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const inputArr = [1, 'a', 'b', 0, 15];
+
+function filterList(arr) {
+	return arr.filter((el) => typeof el === 'number');
+}
+
+const outputArr = filterList(inputArr);
+console.log(outputArr);
---
+++ complete-javascript-course-master/_codewars/maskify.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const inputStr = '4556364607935616';
+
+// function maskify(str) {
+// 	const charsToLeaveUnmasked = 4;
+// 	if (str.length <= charsToLeaveUnmasked) return str;
+// 	const charsToMask = str.length - charsToLeaveUnmasked;
+// 	let maskedStr = '';
+// 	let maskedChars = 0;
+// 	for (let i = 0; i < str.length; i++) {
+// 		maskedChars < charsToMask ? (maskedStr += '#') : (maskedStr += str[i]);
+// 		maskedChars++;
+// 	}
+// 	return maskedStr;
+// }
+
+// function maskify(str) {
+// 	const charsToLeaveUnmasked = 4;
+// 	if (str.length <= charsToLeaveUnmasked) return str;
+// 	const charsToReplace = str.length - charsToLeaveUnmasked;
+// 	let maskedPart = '';
+// 	for (let i = 0; i < charsToReplace; i++) {
+// 		maskedPart += '#';
+// 	}
+// 	return maskedPart + str.slice(charsToReplace);
+// }
+
+function maskify(str) {
+	if (str.length <= 4) return str;
+	return str.slice(0, -4).replace(/./gi, '#') + str.slice(-4);
+}
+
+const maskedStr = maskify(inputStr);
+console.log(maskedStr);
