0bb2afbcf803af7f95e8444f22c780dddbaf2707
Roman Usov <usvbox@yandex.ru>
2021-06-15 22:16:57 +0300

    completed Lesson 127

--- complete-javascript-course-master/10-Functions/starter/script.js
+++ complete-javascript-course-master/10-Functions/starter/script.js
@@ -1,1 +1,22 @@
 'use strict';
+
+const bookings = [];
+const defaultPrice = 199;
+
+const createBooking = function (
+  flightNum,
+  numOfPassengers = 1,
+  price = defaultPrice * numOfPassengers
+) {
+  const booking = {
+    flightNum,
+    numOfPassengers,
+    price,
+  };
+  console.log(booking);
+  bookings.push(booking);
+};
+
+createBooking('LH123');
+createBooking('LH123', 2, 450);
+createBooking('LH123', undefined, 1000);
--- complete-javascript-course-master/_codewars/multiples-of-3-5.js
+++ complete-javascript-course-master/_codewars/multiples-of-3-5.js
@@ -4,33 +4,33 @@ const inputNumber = 56;
 const negativeInput = -5;
 const zeroInput = 0;
 
-// const sumMultiplesOfThreeAndFive = function (number) {
-// 	if (number <= 0) return 0;
-// 	const naturalNumbers = [];
-// 	for (let naturalNum = 1; naturalNum < number; naturalNum++) {
-// 		naturalNumbers.push(naturalNum);
-// 	}
-// 	const divisibleByThree = naturalNumbers.filter((item) => item % 3 === 0);
-// 	const divisibleByFive = naturalNumbers.filter((item) => item % 5 === 0);
-// 	const divisibleByThreeAndFive = [
-// 		...new Set([...divisibleByThree, ...divisibleByFive]),
-// 	].sort((a, b) => a - b);
-// 	const sumOfMultiples = divisibleByThreeAndFive.reduce(
-// 		(total, item) => total + item,
-// 		0
-// 	);
-// 	return sumOfMultiples;
-// };
-
 const sumMultiplesOfThreeAndFive = function (number) {
 	if (number <= 0) return 0;
-	let sumOfMultiples = 0;
+	const naturalNumbers = [];
 	for (let naturalNum = 1; naturalNum < number; naturalNum++) {
-		if (naturalNum % 3 === 0 || naturalNum % 5 === 0)
-			sumOfMultiples += naturalNum;
+		naturalNumbers.push(naturalNum);
 	}
+	const divisibleByThree = naturalNumbers.filter((item) => item % 3 === 0);
+	const divisibleByFive = naturalNumbers.filter((item) => item % 5 === 0);
+	const divisibleByThreeAndFive = [
+		...new Set([...divisibleByThree, ...divisibleByFive]),
+	].sort((a, b) => a - b);
+	const sumOfMultiples = divisibleByThreeAndFive.reduce(
+		(total, item) => total + item,
+		0
+	);
 	return sumOfMultiples;
 };
+
+// const sumMultiplesOfThreeAndFive = function (number) {
+// 	if (number <= 0) return 0;
+// 	let sumOfMultiples = 0;
+// 	for (let naturalNum = 1; naturalNum < number; naturalNum++) {
+// 		if (naturalNum % 3 === 0 || naturalNum % 5 === 0)
+// 			sumOfMultiples += naturalNum;
+// 	}
+// 	return sumOfMultiples;
+// };
 
 //sumMultiplesOfThreeAndFive(inputNumber);
 
