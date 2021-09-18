c8936beb4e3c9a4d2fd7660b97f26687ea47335e
Roman Usov <usvbox@yandex.ru>
2021-09-18 15:33:23 +0300

    studied recursion for fibonacci

--- complete-javascript-course-master/basic-algorithms-and-data-structures/recursive.js
+++ complete-javascript-course-master/basic-algorithms-and-data-structures/recursive.js
@@ -36,7 +36,19 @@ const factorialWithWhile = (n) => {
 // console.log(4 * 3 * 2 * 1);
 
 //// Fibonacci Sequence
+
 // 1, 1, 2, 3, 5, 8, 13, 21
+
+/*
+----------------- CALL STACK ------------------------
+
+//fibonacci(3) = fibonacci(2) + fibonacci(1) = 1 + 1 = 2
+//fibonacci(3) = fibonacci(2) + fibonacci(1) = 1 + 1 = 2
+//fibonacci(4) = fibonacci(3) + fibonacci(2) = 2 + 1 = 3
+fibonacci(5) = fibonacci(4) + fibonacci(3) = 3 + 2 = 5
+-----------------------------------------------------
+*/
+
 const fibonacci = (n) => {
   if (n < 3) return 1;
   return fibonacci(n - 1) + fibonacci(n - 2);
@@ -44,6 +56,20 @@ const fibonacci = (n) => {
 
 const fibResult = fibonacci(5);
 console.log(fibResult);
+
+// const fib = (n) => {
+//   if (n < 3) return 1;
+//   let iMinusTwo = 1; //1) 1 //2) 2 //3) 3
+//   let iMinusOne = 1; //1) 2 //2) 3 //3) 5
+//   for (let i = 3; i <= n; i++) {
+//     const current = iMinusTwo + iMinusOne; //1) 2 //2) 3 //3) 5
+//     iMinusTwo = iMinusOne;
+//     iMinusOne = current;
+//   }
+//   return current;
+// };
+
+console.log(fib(6));
 
 //// Reverse String
 
